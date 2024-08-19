477c98d01f05a97c6b97a9895ef5e960caffac4a
Junior Reis <gilmaikjunior@gmail.com>
2024-08-19 20:47:15 -0300

    deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import { pessoaRouter } from "./src/routes/person.routes.js";
 import { tryConnectSequelize } from "./src/database/db.js";
 const server = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 
 server.use(express.json())
